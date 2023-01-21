2775cf9b8166b9823998c81b728b3ba3bb0dee5e
Faran <faran2093@gmail.com>
2023-01-21 12:33:35 -0600

    Organize code and add css

--- src/App.js
+++ src/App.js
@@ -9,10 +9,21 @@ function App() {
   const [theme, setTheme] = useState(lightTheme)
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <button onClick={() => setTheme(theme === lightTheme ? darkTheme : lightTheme)}>Toggle Theme</button>
-    </ThemeProvider>
+    <div className="app">
+        <ThemeProvider className="toggle-button" theme={theme}>
+          <GlobalStyle />
+          <button onClick={() => setTheme(theme === lightTheme ? darkTheme : lightTheme)}>Toggle Theme</button>
+        </ThemeProvider>
+        
+      <div className="intro-container">
+        <div className="desc-container">
+          <h1>I am a Web Developer!</h1>
+        </div>
+        <div className="img-container"></div>
+      </div>
+      <footer/>
+    </div>
+      
   )
 }
 export default App;
